288cc60a059308cd2296ec082e55befdb26012b1
gogsadmin <RedHat$1>
2018-10-10 16:19:45 +0200

    added terraform

---
+++ desired-state/terraform/variables.tf
@@ -0,0 +1,43 @@
+variable "kubetnetes_version" {
+  type        = "string"
+  description = "The k8s version to deploy eg: '1.8.5', '1.10.7' etc"
+  default     = "1.10.7"
+}
+
+variable "vm_size" {
+  description = "The VM_SKU to use for the agents in the cluster"
+  default     = "Standard_DS2_v2"
+}
+
+variable "node_count" {
+  description = "The number of agents nodes to provision in the cluster"
+  default     = "1"
+}
+
+variable "resource_group_name" {
+  type        = "string"
+  description = "Name of the azure resource group."
+  default     = "testClusterRG"
+}
+
+variable "resource_group_location" {
+  type        = "string"
+  description = "Location of the azure resource group."
+  default     = "westeurope"
+}
+
+variable "linux_admin_username" {
+  type        = "string"
+  description = "User name for authentication to the Kubernetes linux agent virtual machines in the cluster."
+  default     = "terraform"
+}
+
+variable "linux_admin_ssh_publickey" {
+  type        = "string"
+  description = "Configure all the linux virtual machines in the cluster with the SSH RSA public key string. The key should include three parts, for example 'ssh-rsa AAAAB...snip...UcyupgH azureuser@linuxvm'"
+}
+
+variable "sp_least_privilidge" {
+  default     = false
+  description = "[Alpha] This feature creates a limited role for use by the K8s Service principal which limits access to only those resources needed for k8s operation"
+}
---
+++ desired-state/terraform/variables.tfvars
@@ -0,0 +1,2 @@
+linux_admin_username = "YOUR USERNAME"
+linux_admin_ssh_publickey = "YOUR PUB SSH KEY"
